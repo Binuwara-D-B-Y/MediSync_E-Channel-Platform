754bca0242b37315aba4449ad466ea91df163c1b
it23148840 <it23148840@my.sliit.lk>
2025-09-10 21:44:49 +0530

    User Account-management frontend

--- Frontend/medisync/src/App.jsx
+++ Frontend/medisync/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Clienthomepage from './pages/Clienthomepage';
+import UserAccount from './pages/UserAccount';
 // import AdminDashboard from './pages/AdminDashboard'; 
 //import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
         
         {/* Dashboards */}
         <Route path="/patient" element={<Clienthomepage />} />
+        <Route path="/account" element={<UserAccount />} />
         {/* <Route path="/admin" element={<AdminDashboard />} /> */}
 
       </Routes>
