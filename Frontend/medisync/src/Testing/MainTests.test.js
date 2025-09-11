1ec3a9fe46d63a75afbdc5a98f6fc2c2087152f1
it23148840 <140532453+it23148840@users.noreply.github.com>
2025-09-11 22:51:16 +0530

    Delete Frontend/medisync/src/Testing directory

--- Frontend/medisync/src/Testing/MainTests.test.js
+++
@@ -1,6 +0,0 @@
-
-import './WelcomeCard.test.jsx';
-import './FindDoctors.test.jsx';
-import './Clienthomepage.test.jsx';
-
-// This file imports all component tests so you can run them together with your test runner.
