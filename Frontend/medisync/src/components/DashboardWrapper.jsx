bced85b81c4133286568afce09d0bfdfc3a096b5
MAHIMA365 <it23442566@my.sliit.lk>
2025-10-22 22:26:01 +0530

    Add forgot password module

--- Frontend/medisync/src/components/DashboardWrapper.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../styles/DashboardWrapper.css';
-
-export default function DashboardWrapper({ children }) {
-  return <div className="dashboard-wrapper">{children}</div>;
-}
