bd7abbb2eae25e9793c61b91c13da77401039b41
Chamika Lakshan <it23230910@my.sliit.lk>
2025-10-24 05:56:01 +0530

    Added Favorite Doctors Feature

--- wishlist/Frontend/medisync/src/components/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/LoadingSpinner.css';
-
-export default function LoadingSpinner({ size = 'md' }) {
-  return (
-    <div className={`spinner spinner-${size}`} />
-  );
-}
